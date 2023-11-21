de37866880cea4f0a728e35d69a2326da72601a0
Kayke Vêncio <venciokayke@gmail.com>
2023-11-21 13:51:41 -0300

    Primeiro commit

--- Main.java
+++
@@ -1,9 +0,0 @@
-package quiz2periodo2.pkg0;
-
-public class Main {
-    public static void main(String[] args) {
-        QuizGame quizGame = new QuizGame();
-        quizGame.iniciar();
-    }
-}
-    
